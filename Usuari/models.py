89ac990edab682c37ec9503c66b8edb71ac40274
feel1406 <rodrigosaulis@gmail.com>
2014-05-16 14:52:49 +0200

    Modelos html de inicio

--- Usuari/models.py
+++ Usuari/models.py
@@ -1,11 +1,14 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Usuari(models.Model):
-    nom = models.TextField(max_length = 100)
-    adreca = models.TextField(max_length = 150)
-    telefon = models.IntegerField()
+    nom = models.TextField(max_length = 100, blank = False, help_text = "Nom")
+    adreca = models.TextField(max_length = 150, blank = False, help_text = "Adreça")
+    telefon = models.IntegerField(max_length = 12, blank = False, help_text = "Telèfon")
     esAdmin = models.BooleanField()
+    usuari = models.OneToOneField(User)
 
     def __unicode__(self):
         return self.nom
