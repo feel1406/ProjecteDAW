fdd2ab4b241b096d98259fc6bb6e63419efcbbd6
feel1406 <rodrigosaulis@gmail.com>
2014-06-04 18:43:02 +0200

    Creacion JSON comanda Envio JSON a vista

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -92,6 +92,7 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = '/var/www/proyecto/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
